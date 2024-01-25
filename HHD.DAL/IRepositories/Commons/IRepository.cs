4d6ffe6f538935aa96533628aaef7f1d7f30d956
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-25 05:09:35 +0500

    entity repository base created

--- HHD.DAL/IRepositories/Commons/IRepository.cs
+++ HHD.DAL/IRepositories/Commons/IRepository.cs
@@ -1,13 +1,36 @@
 ﻿using HHD.Domain.Entities.Commons;
+using System.Linq.Expressions;
 using System.Security.AccessControl;
 
 namespace HHD.DAL.IRepositories.Commons;
 
 public interface IRepository<TEntity> where TEntity : Auditable
 {
-    ValueTask<bool> DeleteAsync(Guid id);
-    IQueryable<TEntity> SelectAll();
-    ValueTask<TEntity> SelectByIdAsync(Guid id);
-    ValueTask<TEntity> InsertAsync(TEntity entity);
-    ValueTask<TEntity> UpdateAsync(TEntity entity);
+    ValueTask<TEntity> DeleteAsync(
+        Guid id,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    );
+
+    IQueryable<TEntity> SelectAll(
+        Expression<Func<TEntity, bool>>? predicate = default, 
+        bool asNoTracking = false);
+
+    ValueTask<TEntity> SelectByIdAsync(
+        Guid id,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+    );
+
+    ValueTask<TEntity> InsertAsync(
+        TEntity entity,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    );
+
+    ValueTask<TEntity> UpdateAsync(
+        TEntity entity,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    );
 }
---
+++ HHD.DAL/Repositories/Common/Repository.cs
@@ -0,0 +1,93 @@
+﻿using HHD.DAL.DbContexts;
+using HHD.DAL.IRepositories.Commons;
+using HHD.Domain.Entities.Commons;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Security.Principal;
+namespace HHD.DAL.Repositories.Common;
+
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
+{
+    private readonly HHDDbContext _dbContext;
+
+    public Repository(HHDDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        var initialQuery = _dbContext.Set<TEntity>().Where(entity => true);
+
+        if (predicate is not null)
+            initialQuery = initialQuery.Where(predicate);
+
+        if (asNoTracking)
+            initialQuery = initialQuery.AsNoTracking();
+
+        return initialQuery;
+    }
+
+    public async ValueTask<TEntity?> SelectByIdAsync(
+        Guid id,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var initialQuery = _dbContext.Set<TEntity>().Where(entity => true);
+
+        if (asNoTracking)
+            initialQuery = initialQuery.AsNoTracking();
+
+        return await initialQuery.SingleOrDefaultAsync(entity => entity.Id == id, cancellationToken: cancellationToken);
+    }
+
+    public async ValueTask<TEntity> InsertAsync(
+        TEntity entity,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    )
+    {
+        entity.Id = Guid.Empty;
+
+        await _dbContext.Set<TEntity>().AddAsync(entity, cancellationToken);
+
+        if (saveChanges)
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    public async ValueTask<TEntity> UpdateAsync(
+        TEntity entity,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    )
+    {
+        _dbContext.Set<TEntity>().Update(entity);
+
+        if (saveChanges)
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    public async ValueTask<TEntity> DeleteAsync(
+        Guid id,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var entity = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken) ??
+                     throw new InvalidOperationException();
+
+        _dbContext.Set<TEntity>().Remove(entity);
+
+        if (saveChanges)
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+}
